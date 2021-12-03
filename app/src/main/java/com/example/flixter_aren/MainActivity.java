d5a4fb6bf58b9aed310a10e809366e63ae1a5f6e
ArenAshikian <46458166+ArenAshikian@users.noreply.github.com>
2021-12-03 01:12:34 -0800

    Push Flixter Application to GitHub

--- app/src/main/java/com/example/flixter_aren/MainActivity.java
+++ app/src/main/java/com/example/flixter_aren/MainActivity.java
@@ -67,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
                 Log.d(TAG,"onFailure");
+
             }
         });
     }
